28d46ecbe45987d8ff1a04148afa0da681684cb1
CodingCCarpenter <Christineassists@aol.com>
2020-07-17 22:10:50 -0500

    complete base exercise

---
+++ exercise.py
@@ -0,0 +1,12 @@
+is_magician = False
+is_expert = True
+
+# check if magician and expert : print 'you are a master magician'
+if is_magician and is_expert:
+    print('you are a master magician')
+elif is_magician:
+    print('at least you are getting there')
+else:
+    print('you need magic powers')
+# check if magician but not expert : print 'at least you're getting there'
+# check if not a magician : print 'you need magic powers'
