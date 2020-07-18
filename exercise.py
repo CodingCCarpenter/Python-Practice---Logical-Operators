97a347147ecf327a998e96e4790b752e6c546740
CodingCCarpenter <Christineassists@aol.com>
2020-07-17 22:13:02 -0500

    added new elifs for each condition set

--- exercise.py
+++ exercise.py
@@ -1,4 +1,4 @@
-is_magician = False
+is_magician = True
 is_expert = True
 
 # check if magician and expert : print 'you are a master magician'
@@ -10,3 +10,21 @@ else:
     print('you need magic powers')
 # check if magician but not expert : print 'at least you're getting there'
 # check if not a magician : print 'you need magic powers'
+
+is_expert = False
+
+if is_magician and is_expert:
+    print('you are a master magician')
+elif is_magician:
+    print('at least you are getting there')
+else:
+    print('you need magic powers')
+
+is_magician = False
+
+if is_magician and is_expert:
+    print('you are a master magician')
+elif is_magician:
+    print('at least you are getting there')
+else:
+    print('you need magic powers')
